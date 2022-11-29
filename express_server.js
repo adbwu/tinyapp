73fd3d7be457664f633a8a8ef0be89d71ded42a0
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2022-11-29 16:53:04 -0500

    added views/urls_new.js

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -21,6 +23,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
