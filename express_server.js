846ac9d105ae0e9b96f70b94d9980c10a1157fc3
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2022-11-29 16:07:28 -0500

    added header partial in _header.ejs

--- express_server.js
+++ express_server.js
@@ -9,12 +9,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[(req.params.id)] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
